167c4cc0ed63bdb3d3167eabb4a24fd2a14438c4
Roye <tadmoroye@gmail.com>
2021-04-24 14:32:58 +0300

    Implemented data imputation via KNN and XGB for feature selection

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from typing import Dict, List
 from feature import Feature
 import pandas as pd
 from patient import Patient
+from sklearn.impute import KNNImputer
 import numpy as np
+from xgboost import XGBClassifier
 import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime
@@ -21,14 +23,19 @@ def create_patient_list(db):
     """
     hadm_id_list = db.get_hadm_id_list()
     patient_list = []
+    i = 0
     for hadm_id in hadm_id_list:
-        estimated_age,ethnicity,gender,target = db.get_metadata_by_hadm_id(hadm_id)
+        if i == 500:
+            break
+        estimated_age, ethnicity, gender, target = db.get_metadata_by_hadm_id(hadm_id)
         event_list = db.get_events_by_hadm_id(hadm_id)
-        patient = Patient(hadm_id,estimated_age,gender,ethnicity,target,event_list)
+        patient = Patient(hadm_id, estimated_age, gender, ethnicity, target, event_list)
         patient_list.append(patient)
-        return patient_list # Needs to be indented out
+        i += 1
+    return patient_list  # Needs to be indented out
 
-def get_features_for_removal(threshold:float,patient_list:list,db):
+
+def get_features_for_removal(threshold: float, patient_list: list, db):
     """
     Returns a list of features to be removed.
     :param threshold: minimum value of appearances a feature should have
@@ -45,13 +52,14 @@ def get_features_for_removal(threshold:float,patient_list:list,db):
     for patient in patient_list:
         for feature in patient.events:
             if len(patient.events[feature]) > 0:
-                labels_dict[feature] += (1/data_len)
+                labels_dict[feature] += (1 / data_len)
     for label in labels:
-        if(labels_dict[label] < threshold):
+        if (labels_dict[label] < threshold):
             features_to_be_removed.append(label)
     return features_to_be_removed
 
-def remove_features_by_threshold(threshold:float,patient_list:list,db):
+
+def remove_features_by_threshold(threshold: float, patient_list: list, db):
     """
     Removes all features which appear in less than (threshold) of patients
     :param threshold: minimum value of appearances a feature should have
@@ -59,34 +67,32 @@ def remove_features_by_threshold(threshold:float,patient_list:list,db):
     :param db: DB instance
     :return: list of patients after removing irrelevant features
     """
-    features_to_be_removed = get_features_for_removal(threshold,patient_list,db)
+    features_to_be_removed = get_features_for_removal(threshold, patient_list, db)
     for patient in patient_list:
         for feature in patient.events.copy():
             if feature in features_to_be_removed:
                 del patient.events[feature]
     return patient_list
 
+
 def main():
+    X_train = []
+    y_train = []
     db = DB(relevant_events_path)
     patient_list = create_patient_list(db)
-    patient_list = remove_features_by_threshold(threshold,patient_list,db)
-    print(patient_list[0].events)
-    X_train = []
+    patient_list = remove_features_by_threshold(threshold, patient_list, db)
+    for patient in patient_list:
+        y_train.append(patient.target)
+    labels_vector = patient_list[0].create_labels_vector()
     for patient in patient_list:
         vector = patient.create_vector_from_event_list()
         X_train.append(vector)
-        print(vector)
-
-
-
-
-
-
-
-
-
-
-
+    imputer = KNNImputer(n_neighbors=2, weights="uniform")
+    X_train = (imputer.fit_transform(X_train))
+    model = XGBClassifier()
+    model.fit(X_train, y_train)
+    for i in range(len(model.feature_importances_)):
+        print("Feature: %s. Importance: %s"%(labels_vector[i],model.feature_importances_[i]))
 
 
 if __name__ == "__main__":
