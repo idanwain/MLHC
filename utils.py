fbb73171a0387a8a1908f5ed712b0d596b4d0171
idanwain <idanwaintraub11@gmail.com>
2021-05-03 23:18:38 +0300

    change main to utils.py

--- utils.py
+++ utils.py
@@ -1,19 +1,5 @@
-from typing import Dict, List
-from db_interface_eicu import DbEicu
-from feature import Feature
-import pandas as pd
-from patient_mimic import PatientMimic
-from sklearn.impute import KNNImputer
-from sklearn.ensemble import RandomForestClassifier
 import numpy as np
-from xgboost import XGBClassifier
-import time
 from sklearn import metrics
-import matplotlib.pyplot as plt
-import seaborn as sns
-from datetime import datetime
-import sys
-from db_interface_mimic import DbMimic
 
 
 def get_features_for_removal(threshold: float, patient_list: list, db):
@@ -147,49 +133,3 @@ def calc_metrics(y_test, y_score):
     precision,recall,thresholds = metrics.precision_recall_curve(y_test,y_score)
     pr = metrics.auc(recall,precision)
     return roc,pr
-
-
-def main(db, threshold=0.5, n_neighbors=10, xgb_k=50):
-    ### Data preprocessing - cleaning, imputation and vector creation ###
-    start_time = time.time()
-    data = []
-    targets = []
-    patient_list = db.create_patient_list()
-    patient_list = remove_features_by_threshold(threshold, patient_list, db)
-    for patient in patient_list:
-        targets.append(patient.target)
-    labels_vector = patient_list[0].create_labels_vector()
-    for patient in patient_list:
-        vector = patient.create_vector_for_patient()
-        data.append(vector)
-    imputer = KNNImputer(n_neighbors=n_neighbors, weights="uniform")
-    data = (imputer.fit_transform(data))
-
-    ### Feature selection ###
-    model = XGBClassifier()
-    model.fit(data, targets)
-    top_K_xgb = get_top_K_features_xgb(labels_vector, model.feature_importances_.tolist(), k=xgb_k)
-    data = create_vector_of_important_features(data, top_K_xgb)
-
-    for ratio in range(1, 11):
-
-        ### Model fitting ###
-        X_train, y_train, X_test, y_test = split_data(data, targets, ratio)
-        clf_forest = RandomForestClassifier()
-        clf_forest.fit(X_train, y_train)
-        # for i in range(len(X_test)):
-        #     val = clf_forest.predict([X_test[i]])
-        #     print("Predicted: %s. Actual: %s" % (val, y_test[i]))
-        # print("[Sanity] Predicted: %s. Actual: %s" % (clf_forest.predict([(X_train[-1])]), y_train[-1]))
-        # print("Running time: %s" % (time.time() - start_time))
-
-        ### Performance assement ###
-        # err = calc_error(clf_forest, X_test, y_test)
-        roc_val,pr_val = calc_metrics(y_test, clf_forest.predict(X_test))
-        print("AUROC for iteration %s : %s" % (ratio, roc_val))
-        print("AUPR for iteration %s : %s\n" % (ratio, pr_val))
-
-
-
-if __name__ == "__main__":
-    main()
