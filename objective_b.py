03c9fa90b9a0aa6352f83d671521268ab19f4795
Roye <tadmoroye@gmail.com>
2021-06-03 13:32:00 +0300

    Initial usage of hyperopt in model b

--- objective_b.py
+++ objective_b.py
@@ -6,7 +6,9 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+from scipy import stats
 from sklearn.impute import KNNImputer
+from hyperopt import hp, tpe, fmin, Trials
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, VotingClassifier, ExtraTreesClassifier
 import numpy as np
 from xgboost import XGBClassifier
@@ -30,7 +32,7 @@ folds_path = 'C:/tools/folds_mimic_model_b.csv' if user == 'idan' \
     else '/Users/user/Documents/University/Workshop/folds_mimic_model_b.csv'
 
 
-def main(threshold_vals, kNN_vals, XGB_vals, removal_vals, weights):
+def main(params):
     global counter
     counter = 0
     db = DbMimic(boolean_features_path,
@@ -41,110 +43,121 @@ def main(threshold_vals, kNN_vals, XGB_vals, removal_vals, weights):
     folds = db.get_folds()
     patient_list_base = db.create_patient_list()
 
-    for product in itertools.product(kNN_vals, XGB_vals, threshold_vals, removal_vals, weights):
-        start_time = time.time()
-        data = []
-        targets = []
-        folds_indices = []
-        auroc_vals = []
-        aupr_vals = []
-        patient_list = copy.deepcopy(patient_list_base)
+    # for product in itertools.product(kNN_vals, XGB_vals, threshold_vals, removal_vals, weights):
+    start_time = time.time()
+    data = []
+    targets = []
+    folds_indices = []
+    auroc_vals = []
+    aupr_vals = []
+    patient_list = copy.deepcopy(patient_list_base)
 
-        ### Hyperparameters ###
-        threshold = product[2]  # Minimum appearance for feature to be included
-        n_neighbors = product[0]  # Neighbors amount for kNN
-        xgb_k = product[1]  # Amount of features to return by XGB
-        removal_factor = product[3]  # How many negative samples we remove from the training set.
-        weight = product[4]
-        config = {
-            "Threshold": threshold,
-            "kNN": n_neighbors,
-            "k_XGB": xgb_k,
-            "Removal factor": removal_factor,
-            'weights': weight
+    ### Hyperparameters ###
+    # threshold = product[2]  # Minimum appearance for feature to be included
+    # n_neighbors = product[0]  # Neighbors amount for kNN
+    # xgb_k = product[1]  # Amount of features to return by XGB
+    # removal_factor = product[3]  # How many negative samples we remove from the training set.
+    # weight = product[4]
+    threshold = params['threshold_vals']  # Minimum appearance for feature to be included
+    n_neighbors = params['kNN_vals']  # Neighbors amount for kNN
+    xgb_k = params['XGB_vals']  # Amount of features to return by XGB
+    removal_factor = [0]  # How many negative samples we remove from the training set.
+    weight = [1,1,1]
+    config = {
+        "Threshold": threshold,
+        "kNN": n_neighbors,
+        "k_XGB": xgb_k,
+        "Removal factor": removal_factor,
+        'weights': weight
+    }
+    utils.log_dict(vals=config, msg="Configuration:")
+
+    patient_list, removed_features = utils.remove_features_by_threshold(threshold, patient_list, db)
+    for patient in patient_list:
+        targets.append(patient.target)
+        for fold in folds:
+            if str(patient.hadm_id) in (folds[fold]):
+                folds_indices.append(fold)
+
+    labels_vector = utils.create_labels_vector(db, removed_features)
+    for patient in patient_list:
+        vector = patient.create_vector_for_patient()
+        data.append(vector)
+
+    ### Data imputation ###
+    imputer = KNNImputer(n_neighbors=n_neighbors, weights="uniform")
+    data = (imputer.fit_transform(data))
+
+    for test_fold in folds:
+        ### Data split ###
+        X_train, y_train, X_test, y_test = utils.split_data_by_folds(data, targets, folds_indices, test_fold)
+
+        ### Feature selection ###
+        model = XGBClassifier(use_label_encoder=False)
+        model.fit(np.asarray(X_train), y_train)
+        top_K_xgb = utils.get_top_K_features_xgb(labels_vector, model.feature_importances_.tolist(), k=xgb_k)
+        X_train = utils.create_vector_of_important_features(X_train, top_K_xgb)
+        X_test = utils.create_vector_of_important_features(X_test, top_K_xgb)
+
+        ### Class balancing ###
+        # over_balancer = BorderlineSMOTE()
+        # X_train, y_train = over_balancer.fit_resample(X_train, y_train)
+        under_balancer = TomekLinks()
+        X_train, y_train = under_balancer.fit_resample(X_train, y_train)
+
+        ### Model fitting ###
+        clf1 = RandomForestClassifier()
+        # clf2 = KNeighborsClassifier()
+        clf = VotingClassifier(estimators=[('rf', clf1)], voting='soft') #,weights=weight
+        params = {
+            'rf__n_estimators': [50, 175],
+            'rf__max_depth':[5,25]
+            # 'rf__random_state': [0, 2],
+            # 'knn__n_neighbors': [7, 12],
+            # 'knn__leaf_size': [22, 40]
         }
-        utils.log_dict(vals=config, msg="Configuration:")
+        grid = GridSearchCV(estimator=clf, param_grid=params)
+        grid = grid.fit(X_train, y_train)
 
-        patient_list, removed_features = utils.remove_features_by_threshold(threshold, patient_list, db)
-        for patient in patient_list:
-            targets.append(patient.target)
-            for fold in folds:
-                if str(patient.hadm_id) in (folds[fold]):
-                    folds_indices.append(fold)
+        ### Performance assement ##
+        roc_val, ns_fpr, ns_tpr, lr_fpr, lr_tpr = utils.calc_metrics_roc(grid, X_test, y_test)
+        pr_val, no_skill, lr_recall, lr_precision = utils.calc_metrics_pr(grid, X_test, y_test)
+        auroc_vals.append([roc_val, ns_fpr, ns_tpr, lr_fpr, lr_tpr])
+        aupr_vals.append([pr_val, no_skill, lr_recall, lr_precision])
 
-        labels_vector = utils.create_labels_vector(db, removed_features)
-        for patient in patient_list:
-            vector = patient.create_vector_for_patient()
-            data.append(vector)
+    # utils.plot_graphs(auroc_vals, aupr_vals, counter, 'b')
 
-        ### Data imputation ###
-        imputer = KNNImputer(n_neighbors=n_neighbors, weights="uniform")
-        data = (imputer.fit_transform(data))
+    counter += 1
 
-        for test_fold in folds:
-            ### Data split ###
-            X_train, y_train, X_test, y_test = utils.split_data_by_folds(data, targets, folds_indices, test_fold)
-
-            ### Feature selection ###
-            model = XGBClassifier(use_label_encoder=False)
-            model.fit(np.asarray(X_train), y_train)
-            top_K_xgb = utils.get_top_K_features_xgb(labels_vector, model.feature_importances_.tolist(), k=xgb_k)
-            X_train = utils.create_vector_of_important_features(X_train, top_K_xgb)
-            X_test = utils.create_vector_of_important_features(X_test, top_K_xgb)
-
-            ### Class balancing ###
-            # over_balancer = BorderlineSMOTE()
-            # X_train, y_train = over_balancer.fit_resample(X_train, y_train)
-            under_balancer = TomekLinks()
-            X_train, y_train = under_balancer.fit_resample(X_train, y_train)
-
-            ### Model fitting ###
-            clf1 = DecisionTreeClassifier()
-            clf2 = RandomForestClassifier()
-            clf3 = KNeighborsClassifier()
-            clf = VotingClassifier(estimators=[('dt', clf1), ('rf', clf2), ('knn', clf3)], voting='soft',
-                                   weights=weight)
-            params = {
-                'rf__n_estimators': [50, 175],
-                'rf__random_state': [0, 2],
-                'dt__random_state': [0, 2],
-                'dt__max_depth': [5, 20],
-                'knn__n_neighbors': [7, 12],
-                'knn__leaf_size': [22, 40]
-            }
-            grid = GridSearchCV(estimator=clf, param_grid=params)
-            grid = grid.fit(X_train, y_train)
-
-            ### Performance assement ##
-            roc_val, ns_fpr, ns_tpr, lr_fpr, lr_tpr = utils.calc_metrics_roc(grid, X_test, y_test)
-            pr_val, no_skill, lr_recall, lr_precision = utils.calc_metrics_pr(grid, X_test, y_test)
-            auroc_vals.append([roc_val, ns_fpr, ns_tpr, lr_fpr, lr_tpr])
-            aupr_vals.append([pr_val, no_skill, lr_recall, lr_precision])
-
-        utils.plot_graphs(auroc_vals, aupr_vals, counter, 'b')
-
-        counter += 1
-
-        utils.log_dict(vals={"AUROC_AVG": np.average([i[0] for i in auroc_vals]),
-                             "AUPR_AVG": np.average([i[0] for i in aupr_vals]),
-                             "AUROC_STD": np.std([i[0] for i in auroc_vals]),
-                             "AUPR_STD": np.std([i[0] for i in aupr_vals])}, msg="Run results:")
-        utils.log_dict(msg="Running time: " + str(time.time() - start_time))
-        return np.average([i[0] for i in auroc_vals]) + np.average([i[0] for i in aupr_vals]), counter - 1
+    utils.log_dict(vals={"AUROC_AVG": np.average([i[0] for i in auroc_vals]),
+                         "AUPR_AVG": np.average([i[0] for i in aupr_vals]),
+                         "AUROC_STD": np.std([i[0] for i in auroc_vals]),
+                         "AUPR_STD": np.std([i[0] for i in aupr_vals])}, msg="Run results:")
+    utils.log_dict(msg="Running time: " + str(time.time() - start_time))
+    # return np.average([i[0] for i in auroc_vals]) + np.average([i[0] for i in aupr_vals]), counter - 1
+    return -1.0 * np.average([i[0] for i in auroc_vals])
 
 
 if __name__ == "__main__":
-    threshold_vals = []
-    kNN_vals = []
-    XGB_vals = []
-    removal_vals = []
-    weights = [list(a) for a in list(itertools.product(range(1, 3), range(12, 15), range(1, 3)))]
-    for i in range(3, 9):
-        XGB_vals.append(40 + (i * 2))
-    for i in range(7, 15):
-        kNN_vals.append(i)
-    for i in range(1, 3):
-        removal_vals.append(5 / (10 + i))
-    for i in range(0, 10):
-        threshold_vals.append(0.1 * i)
-    main([0.2], [8], [48], [0.0], [[2, 13, 1]])
+    # threshold_vals = []
+    # kNN_vals = []
+    # XGB_vals = []
+    # removal_vals = []
+    # weights = [list(a) for a in list(itertools.product(range(1, 3), range(12, 15), range(1, 3)))]
+    # for i in range(3, 9):
+    #     XGB_vals.append(40 + (i * 2))
+    # for i in range(7, 15):
+    #     kNN_vals.append(i)
+    # for i in range(1, 3):
+    #     removal_vals.append(5 / (10 + i))
+    # for i in range(0, 10):
+    #     threshold_vals.append(0.1 * i)
+    # main([0.2], [8], [48], [0.0], [[2, 13, 1]])
+    space = {
+        'threshold_vals': hp.uniform('thershold_val',0.1,1),
+        'kNN_vals': hp.choice('kNN_vals',range(1,11)),
+        'XGB_vals': hp.choice('XGB_vals',range(30,62,2)),
+    }
+    trials = Trials()
+    best = fmin(fn=main,space=space,algo=tpe.suggest,max_evals=10,trials=trials)
+    print(best)
