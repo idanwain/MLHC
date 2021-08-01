efa1858038c645e2b1bda3b9c5ab88367f05ddb6
Roye <tadmoroye@gmail.com>
2021-08-01 14:15:09 +0300

    Add module 3

--- module_3_model.py
+++ module_3_model.py
@@ -3,8 +3,17 @@ import pandas as pd
 
 path_to_model = "./model"
 
-def module_3_model(processed_external_validation_set_path,model_type):
+
+def module_3_model(processed_external_validation_set_path, model_type):
+    path_to_risk_score = 'model_' + model_type + '_mimic_cohort_risk_score_group_8.csv'
+    final_df = {'identifier': [], 'risk_score': []}
     df = pd.read_csv(processed_external_validation_set_path)
+    for row in df.iterrows():
+        final_df['identifier'].append(row[1]['identifier'])
+    df.drop('identifier')
     data = df.to_numpy()
-    clf = pickle.load(path_to_model)
+    clf = pickle.load(path_to_model + '_' + model_type)
     result = clf.predict_proba(data)
+    final_df['risk_score'].extend(result)
+    res = pd.DataFrame.from_dict(final_df)
+    res.to_csv(path_to_risk_score)
