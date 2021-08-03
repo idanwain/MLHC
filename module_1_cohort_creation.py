0ac26fcf4030f3ab1a07dae2a12d7b8c51a345bd
idanwain <idanwaintraub11@gmail.com>
2021-08-03 22:11:03 +0300

    training data adjustments

--- module_1_cohort_creation.py
+++ module_1_cohort_creation.py
@@ -7,14 +7,22 @@ if os.name == 'posix':
 else:
     user = 'idan'
 
+training = False
+
 
 def module_1_cohort_creation(file_path, db_conn, model_type):
-    db = sql_helper.SqlHelper(db_conn, user)
-    db.load_cohort_to_db(file_path, model_type)
+    db = sql_helper.SqlHelper(db_conn, model_type, user, training)
+    print('1')
+    db.load_cohort_to_db(file_path)
+    print('2')
     db.create_features_table()
-    db.init_boolean_features(model_type, user)
-    db.create_symptoms(model_type)
-    db.merge_features_and_cohort(model_type, user)
+    print('3')
+    db.init_boolean_features()
+    print('4')
+    db.create_symptoms()
+    print('5')
+    db.merge_features_and_cohort()
+    print('6')
     db.close()
     db_conn.close()
 
@@ -23,11 +31,21 @@ def module_1_cohort_creation(file_path, db_conn, model_type):
 
 
 if __name__ == '__main__':
-    db_conn = psycopg2.connect(
-        host="localhost",
-        database="mimic",
-        user="postgres",
-        password="")
-    model_type = 'a'
+    if user == 'idan':
+        db_conn = psycopg2.connect(
+            host="localhost",
+            database="mimic",
+            user="postgres",
+            password="")
+    else:
+        db_conn = psycopg2.connect(
+            host="localhost",
+            database="mimic",
+            user="mimicuser",
+            password="",
+            options="--search_path=mimiciii"
+        )
+    model_type = 'b'
+    training = True
     path = f"'C:/tools/model_{model_type}_mimic_cohort.csv'" if user == 'idan' else f"'/Users/user/Documents/University/Workshop/model_{model_type}_mimic_cohort.csv'"
     module_1_cohort_creation(path, db_conn, model_type)
